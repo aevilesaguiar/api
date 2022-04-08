f4a1d94e9f06741925ec632996bac9d28527a693
aevilesaguiar <aeviles@hotmail.com>
2022-04-08 11:45:36 -0300

    classe usuario

--- src/main/java/br/com/aeviles/api/ApiApplication.java
+++ src/main/java/br/com/aeviles/api/ApiApplication.java
@@ -1,5 +1,6 @@
 package br.com.aeviles.api;
 
+import br.com.aeviles.api.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,14 @@ public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+
+
+		//Verificar se as anotações realmente estão sendo contruidas, a anotação no @argsContructor está funcionando
+
+		User user = new User(1, "Aeviles", "aev@aev.com","12345678");
 	}
+
+
+
 
 }
