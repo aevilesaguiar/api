a3b6e7b9b796f967fd4a465fa694c5667af4666c
aevilesaguiar <aeviles@hotmail.com>
2022-04-09 11:58:28 -0300

    interface e class  user service

---
+++ src/main/java/br/com/aeviles/api/services/UserService.java
@@ -0,0 +1,8 @@
+package br.com.aeviles.api.services;
+
+import br.com.aeviles.api.model.User;
+
+public interface UserService {
+
+    User findById (Integer id);
+}
---
+++ src/main/java/br/com/aeviles/api/services/impl/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package br.com.aeviles.api.services.impl;
+
+import br.com.aeviles.api.model.User;
+import br.com.aeviles.api.repository.UserRepository;
+import br.com.aeviles.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+
+    @Override
+    public User findById(Integer id) {
+
+        //é utilizado o Optional por que ele pode retornar ou não um ID
+        Optional<User> obj = repository.findById(id);
+        return obj.orElse(null); //senão encontrar o Id o valor é nulo
+
+    }
+
+
+
+}
