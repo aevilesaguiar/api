d5dc03e138fc8b5f2944af86da55b50a0ecee26a
aevilesaguiar <aeviles@hotmail.com>
2022-04-09 12:55:22 -0300

    retornando Dto para UserController

--- src/main/java/br/com/aeviles/api/controller/UserController.java
+++ src/main/java/br/com/aeviles/api/controller/UserController.java
@@ -1,7 +1,9 @@
 package br.com.aeviles.api.controller;
 
 import br.com.aeviles.api.model.User;
+import br.com.aeviles.api.model.dto.UserDto;
 import br.com.aeviles.api.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserController {
 
+    //instancia do model Mapper que foi defiidio na classe ModelMapperConfig
+    @Autowired
+    private ModelMapper mapper;
+
     @Autowired
     private UserService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDto> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDto.class));
 
     }
 
--- src/main/java/br/com/aeviles/api/model/dto/UserDto.java
+++ src/main/java/br/com/aeviles/api/model/dto/UserDto.java
@@ -1,5 +1,6 @@
 package br.com.aeviles.api.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,7 @@ public class UserDto {
     private Integer id;
     private String name;
     private String email;
+    @JsonIgnore
     private String password;
 
 }
