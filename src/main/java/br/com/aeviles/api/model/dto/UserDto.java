7396ea10102473dceb89ae9ee2c715b52cb1a34b
aevilesaguiar <aeviles@hotmail.com>
2022-04-09 12:45:33 -0300

    classe UserDto

---
+++ src/main/java/br/com/aeviles/api/model/dto/UserDto.java
@@ -0,0 +1,19 @@
+package br.com.aeviles.api.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+
+}
