3a60ed6e4c78a9b9438ad5f32af30f8d1fe7ff86
aevilesaguiar <aeviles@hotmail.com>
2022-04-11 15:22:24 -0300

    teste para classe principal

--- src/main/java/br/com/aeviles/api/ApiApplication.java
+++ src/main/java/br/com/aeviles/api/ApiApplication.java
@@ -10,13 +10,5 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 
-
-		//Verificar se as anotações realmente estão sendo contruidas, a anotação no @argsContructor está funcionando corretamente
-
-		User user = new User(1, "Aeviles", "aev@aev.com","12345678");
 	}
-
-
-
-
 }
